5d5707706d888d64c24671470ad7ad8c908cfe5a
modobasa <modobasa@pentalog.com>
2022-02-14 12:37:46 +0200

    Updated! Added wait_for_db + Travis_ci - ver 2

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\modobasa\Documents\Django\Udemy\Rest API\recipe-app-api\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
