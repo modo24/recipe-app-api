02a099d05cd23dd3ff74de9f599f1e1f622dca1b
modobasa <modobasa@pentalog.com>
2022-02-15 11:56:05 +0200

    Added working list tags endpoint.

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,5 +21,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/', include('user.urls'))
+    path('api/user/', include('user.urls')),
+    path('api/recipe/', include('recipe.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
