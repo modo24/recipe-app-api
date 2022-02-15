7ec9617a0d04e4b95ad5c0e804b04852a8b64679
modobasa <modobasa@pentalog.com>
2022-02-15 09:52:14 +0200

    Added endpoint for creating users

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/user/', include('user.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
